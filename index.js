6840e70f0cf899b286507d31891544c76f3637c0
marysclair <ramalho.maria@academico.ifpb.edu.br>
2025-07-07 21:26:27 -0300

    minor corrections on server

--- index.js
+++ index.js
@@ -140,6 +140,6 @@ const http = require("http");
 const server = http.createServer(app);
 server.setTimeout(10 * 60 * 1000);
 
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${port}`);
 });
